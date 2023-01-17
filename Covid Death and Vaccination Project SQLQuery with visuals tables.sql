16b4babee72fed3516f6e1bcd0f0f2896bd5561e
Jesse Richardville <jesse.richardville@gmail.com>
2023-01-17 09:14:34 -0600

    initial commit

---
+++ Covid Death and Vaccination Project SQLQuery with visuals tables.sql
@@ -0,0 +1,159 @@
+--SQL Query for Covid Deaths and Vaccinations
+
+SELECT *
+FROM [Covid Portfolio Project]..CovidDeaths
+WHERE continent is not null
+ORDER BY 3,4
+
+--SELECT *
+--FROM [Covid Portfolio Project]..CovidVaccinations
+--ORDER BY 3,4
+
+--Select Data to be used
+
+SELECT Location, date, total_cases, new_cases, total_deaths, population
+FROM [Covid Portfolio Project]..CovidDeaths
+WHERE continent is not null
+ORDER BY 1, 2
+
+--Total Cases vs Total Deaths
+SELECT Location, date, total_cases, total_deaths, (total_deaths/total_cases) * 100 as DeathPercentage
+FROM [Covid Portfolio Project]..CovidDeaths
+WHERE location LIKE '%states%'
+and continent is not null
+ORDER BY 1, 2
+
+--Total Cases vs Population, % of population that contracted Covid
+SELECT Location, date, population, total_cases, (total_cases/population) * 100 as ContractedCovid
+FROM [Covid Portfolio Project]..CovidDeaths
+WHERE location LIKE '%states%'
+and continent is not null
+ORDER BY 1, 2
+
+--Countries with the highest infection rate compared to population
+SELECT continent, population, MAX(total_cases) as HighestInfectionCount, MAX((total_cases/population)) * 100 as PercentContractedCovid
+FROM [Covid Portfolio Project]..CovidDeaths
+WHERE continent is not null
+--WHERE location LIKE '%states%'
+GROUP BY continent, population
+ORDER BY PercentContractedCovid DESC
+
+--Countries with the Highest Death Count per Population
+SELECT Location, MAX(CAST(Total_deaths as int)) as TotalDeathCount
+FROM [Covid Portfolio Project]..CovidDeaths
+WHERE continent is not null
+GROUP BY Location
+ORDER BY TotalDeathCount DESC
+
+--Location with the Highest Death Count per Population
+SELECT location, MAX(CAST(Total_deaths as int)) as TotalDeathCount
+FROM [Covid Portfolio Project]..CovidDeaths
+WHERE continent is null
+GROUP BY location
+ORDER BY TotalDeathCount DESC
+
+--Continents with the Highest Death Count per Population
+SELECT continent, MAX(CAST(Total_deaths as int)) as TotalDeathCount
+FROM [Covid Portfolio Project]..CovidDeaths
+WHERE continent is not null
+GROUP BY continent
+ORDER BY TotalDeathCount DESC
+
+--Global Number
+SELECT date, SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 as DeathPercentage
+FROM [Covid Portfolio Project]..CovidDeaths
+WHERE continent is not null
+--GROUP BY date
+ORDER BY 1, 2
+
+SELECT SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 as DeathPercentage
+FROM [Covid Portfolio Project]..CovidDeaths
+WHERE continent is not null
+--GROUP BY date
+ORDER BY 1, 2
+
+--Total Population vs Vaccination
+WITH PopvsVac(Continent, Location, Date, Population, new_vaccinations, RollingPeopleVaccinated)
+as
+(
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(CONVERT(bigint, vac.new_vaccinations)) 
+	OVER (Partition by dea.location ORDER BY dea.location, dea.date) as RollingPeopleVaccinated
+--	,( RollingPeopleVaccinated/population) *100
+FROM [Covid Portfolio Project]..CovidDeaths dea
+JOIN [Covid Portfolio Project]..CovidVaccinations vac
+	ON dea.location = vac.location
+	and dea.date = vac.date
+WHERE dea.continent is not null
+--ORDER BY 2, 3
+)
+SELECT *, (RollingPeopleVaccinated/Population) *100
+FROM PopvsVac
+
+
+--use CTE
+WITH PopvsVac(Continent, Location, Date, Population, RollingPeopleVaccinated)
+
+
+--temp table
+
+DROP TABLE IF exists #PercentPopulationVaccinated
+CREATE TABLE #PercentPopulationVaccinated
+(
+Continent nvarchar(255),
+Location nvarchar(255),
+Date datetime,
+Population numeric,
+New_vaccinations numeric,
+RollingPeopleVaccinated numeric
+)
+INSERT INTO #PercentPopulationVaccinated
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(CONVERT(bigint, vac.new_vaccinations)) 
+	OVER (Partition by dea.location ORDER BY dea.location, dea.date) as RollingPeopleVaccinated
+--	,( RollingPeopleVaccinated/population) *100
+FROM [Covid Portfolio Project]..CovidDeaths dea
+JOIN [Covid Portfolio Project]..CovidVaccinations vac
+	ON dea.location = vac.location
+	and dea.date = vac.date 
+--WHERE dea.continent is not null
+--ORDER BY 2, 3
+SELECT *, (RollingPeopleVaccinated/Population) *100
+FROM #PercentPopulationVaccinated
+
+--Creating View to Store Data for Visualization
+
+CREATE VIEW PercentPopulationVaccinated as 
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(CONVERT(bigint, vac.new_vaccinations)) 
+	OVER (Partition by dea.location ORDER BY dea.location, dea.date) as RollingPeopleVaccinated
+--	,( RollingPeopleVaccinated/population) *100
+FROM [Covid Portfolio Project]..CovidDeaths dea
+JOIN [Covid Portfolio Project]..CovidVaccinations vac
+	ON dea.location = vac.location
+	and dea.date = vac.date 
+WHERE dea.continent is not null
+--ORDER BY 2, 3
+
+SELECT *
+FROM PercentPopulationVaccinated
+
+--These are taken out since they are not included in the above queries, EU is part of Europe
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From [Covid Portfolio Project]..CovidDeaths
+--Where location like '%states%'
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From [Covid Portfolio Project]..CovidDeaths
+--Where location like '%states%'
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From [Covid Portfolio Project]..CovidDeaths
+--Where location like '%states%'
+Group by Location, Population, date
+order by PercentPopulationInfected desc
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Covid Infections and Deaths - Data Exploration
+
+## Intro:
+This data exploration project was completed by Jesse Richardville as a portfolio project looking into the rate of infection and death rate by country and region throughout the world. SQL and Tableau have been used to complete this data exploration project, which has been posted online through GitHub.
+
+## Summary:
+
+Covid data has been collected by [Johns Hopkins University](https://ourworldindata.org/covid-deaths#confirmed-deaths-and-cases-our-data-source) and made available by [Our World In Data](https://ourworldindata.org/covid-deaths#explore-the-global-data-on-confirmed-covid-19-deaths) since the beginning of the pandemic and continues to be updated daily. This data exploration project focuses on Covid infection and death data from 1/1/2020 - 3/30/2021. All the data can be found [here](https://ourworldindata.org/explorers/coronavirus-data-explorer) and has been made available by [Our World In Data](https://ourworldindata.org/coronavirus) with this free to use [license](https://ourworldindata.org/covid-deaths#licence). Many people and groups have contributed to the data and the following must be acknowledged to [cite their work](https://ourworldindata.org/covid-deaths#acknowledgements):
+
+Edouard Mathieu, Hannah Ritchie, Lucas Rodés-Guirao, Cameron Appel, Charlie Giattino, Joe Hasell, Bobbie Macdonald, Saloni Dattani, Diana Beltekian, Esteban Ortiz-Ospina and Max Roser (2020) - "Coronavirus Pandemic (COVID-19)". Published online at OurWorldInData.org. Retrieved from: 'https://ourworldindata.org/coronavirus' [Online Resource]
+
